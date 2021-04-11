ee2546fc29fa581dfcd4e6c913155acae7898409
Michael A. Plikk <michael@plikk.com>
2021-04-11 11:42:59 +0200

    Add docker cleanup alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -68,3 +68,8 @@ mem() {
 gitdiff() {
     git diff --name-only --diff-filter=d 2>/dev/null | xargs bat --diff
 }
+
+dcleanup(){
+    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+}
